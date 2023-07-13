31fdd089756ea1059086218572878a80a84e1b6b
0xd3lbow <130616587+0xd3lbow@users.noreply.github.com>
2023-07-13 17:44:48 -0400

    Fixed an issue with the x-axis

--- src/spread_plot.py
+++ src/spread_plot.py
@@ -1,5 +1,4 @@
 from matplotlib.animation import FuncAnimation
-from matplotlib.dates import date2num
 import matplotlib.ticker as ticker
 import matplotlib.pyplot as plt
 import threading
@@ -32,7 +31,7 @@ def on_message(ws, message):
             timestamps.append(message['time'])
             spread_values.append(spread)
         else:
-            print('Bid and ask prices not found in the message.')
+            print('Bid-Ask prices not found in the WebSocket message.')
 
 def on_error(ws, error):
     print('WebSocket error:', error)
@@ -51,15 +50,40 @@ line, = ax.plot([], [])
 ax.set_xlabel('Time')
 ax.set_ylabel('Spread')
 ax.set_title('Bid-Ask Spread')
+plt.grid(True)
 
 formatter = ticker.FuncFormatter(lambda x, _: '${:,.2f}'.format(x))
 ax.yaxis.set_major_formatter(formatter)
 
+xticks = []
+xticklabels = []
+
+max_ticks = 10
+
 def update_plot(frame):
-    x = [date2num(datetime.datetime.strptime(t, '%Y-%m-%dT%H:%M:%S.%fZ')) for t in timestamps]
-    line.set_data(x, spread_values)
-    ax.relim()
-    ax.autoscale_view()
+    datetime_timestamps = [datetime.datetime.strptime(t, '%Y-%m-%dT%H:%M:%S.%fZ') for t in timestamps]
+
+    if len(datetime_timestamps) == len(spread_values):
+        line.set_data(datetime_timestamps, spread_values)
+        ax.relim()
+        ax.autoscale_view()
+
+        if not xticks or len(xticks) != len(datetime_timestamps) or datetime_timestamps[-1] != xticks[-1]:
+            xticks.clear()
+            xticklabels.clear()
+
+            if len(datetime_timestamps) <= max_ticks:
+                xticks.extend(datetime_timestamps)
+                xticklabels.extend(datetime_timestamps)
+            else:
+                indices = [int(i * (len(datetime_timestamps) - 1) / (max_ticks - 1)) for i in range(max_ticks)]
+                xticks.extend([datetime_timestamps[i] for i in indices])
+                xticklabels.extend([datetime_timestamps[i] for i in indices])
+
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(xticklabels, rotation=45, ha='right', rotation_mode='anchor')
+        ax.xaxis.set_major_locator(ticker.MaxNLocator(nbins='auto', integer=True))
+
     return line,
 
 ani = FuncAnimation(fig, update_plot, interval=1000)
